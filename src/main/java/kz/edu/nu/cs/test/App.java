af8957cdca7ae44f7cb5a75f0a948b3b2580f55e
marks <marks1024@users.noreply.github.com>
2018-08-23 15:37:22 +0600

    first commit

---
+++ src/main/java/kz/edu/nu/cs/test/App.java
@@ -0,0 +1,36 @@
+package kz.edu.nu.cs.test;
+
+/**
+ * App is generated as part of the testexercise361 archetype. This
+ * archetype generates a maven project with some more up-to-date features
+ * compared with the org.apache quickstart archetype.
+ * <p>
+ * Some of the changes of this archetype
+ * <ul>
+ * <li>Version 4.12 of JUnit
+ * <li>Build with Java 8
+ * <li>Failing unit test
+ * </ul>
+ * <p>
+ * If you have obtained this project then you can build and install to your
+ * local maven repository. To build the project, go to the project root and run
+ * <code>mvn archetype:create-from-project</code>.
+ * <p>
+ * After building, navigate to <code>target/generated-sources/archetype</code>
+ * and run <code>mvn install</code>. This will add the archetype to your local
+ * maven repository.
+ * <p> 
+ * Add <code>.m2/archetype-catalog.xml</code> as a local catalog in eclipse 
+ * 
+ * @author marks1024
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+    
+    public static int testMethod() {
+        return 1;
+    }
+}
---
+++ src/test/java/kz/edu/nu/cs/test/AppTest.java
@@ -0,0 +1,14 @@
+package kz.edu.nu.cs.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+    
+    @Test
+    public void test() {
+        assertEquals("Test Method", App.testMethod(), 1);
+    }
+    
+}
